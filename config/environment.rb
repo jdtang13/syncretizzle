63bae904a7c0ce83141e58b04134be77ed7af3cf
Jonathan Tang <jdtang13@gmail.com>
2015-02-21 06:38:57 -0500

    Add devise views

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+#config.action_mailer.default_url_options = { :host => 'localhost:3000' }
