e13054e1fc61d63a4ddf3dc6dfe962ae71ecaffd
Andrew Luo <andrewjluo@gmail.com>
2015-02-21 17:16:03 -0800

    Voting working

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,10 +3,10 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(function(){
-	_.templateSettings = {
-		interpolate: /\{\{\=(.+?)\}\}/g,
-		evaluate: /\{\{(.+?)\}\}/g
-	};
+	// _.templateSettings = {
+	// 	interpolate: /\{\{\=(.+?)\}\}/g,
+	// 	evaluate: /\{\{(.+?)\}\}/g
+	// };
 
 	SyncApp = {};
 	window.SyncApp = SyncApp;
@@ -16,10 +16,10 @@ $(function(){
 	//Global pubSub object
 	Backbone.pubSub = _.extend({}, Backbone.Events);
 
+	//Backbone Model, contains room_id and segmentList
 	Room = Backbone.Firebase.Model.extend({
 		initialize: function(room_id) {
 			this.room_id = room_id;
-			this.segments = new TextSegmentList({'room_id': this.room_id});
 		},
 		//Return the url specific to the room
 		url: function() {
@@ -27,55 +27,96 @@ $(function(){
 		}
 	});
 
-	TextSegment = Backbone.Model.extend({
+	//Segment list, contains vote indices, knows room id and model
+	TextSegmentList = Backbone.Firebase.Model.extend({
+		el: "#text_boxes",
 		defaults: function() {
 			return {
-				text: "",
-				votes: 0
+				votes: [0, 0, 0, 0, 0, 0]
 			}
-		}
-	});
-
-	TextSegmentList = Backbone.Firebase.Collection.extend({
-		model: TextSegment,
+		},
 		initialize: function(options) {
 			this.room_id = room_id;
+			this.model = options.model;
 		},
 		url: function() {
 	      return new Firebase(BASE_FIREBASE_URL + "room-" + this.room_id + "/text_segments");
 		}
 	});
 
+	//View for rendering the segment list
 	TextSegmentListContainer = Backbone.View.extend({
-		events: {},
+		events: {
+			'click a#upvote_btn': 'handleUpClick',
+			'click a#downvote_btn': 'handleDownClick'
+		},
 		initialize: function(options) {
 			this.model = options.model;
+			this.list_model = options.list_model;
 			this.segments = options.segments;
 			this.template = _.template($("#tpl-text-item").html());
 			this.$el = $("#text_boxes");
+			this.curr_url = BASE_FIREBASE_URL + "room-" + this.model.room_id + "/text_segments/votes/";
+		},
+		handleUpClick: function(e) {
+			var $item = $(e.target).closest("div");
+			if($item.hasClass("btn--disabled")) {
+				return false;
+			}
+			$item.addClass("btn--disabled");
+			var idName = $item.attr('id');
+			var index = parseInt(idName.slice(-1));
+			var that = this;
+			var votesRef = new Firebase(this.curr_url + index);
+			votesRef.transaction(function(data) {
+				if(data === parseInt(data, 10)) {
+					return data + 1;
+				}
+			});
+		},
+		handleDownClick: function(e) {
+			var $item = $(e.target).closest("div");
+			if($item.hasClass("btn--disabled")) {
+				return false;
+			}
+			var idName = $item.attr('id');
+			var index = parseInt(idName.slice(-1));
+			var that = this;
+			var votesRef = new Firebase(this.curr_url + index);
+			votesRef.transaction(function(data) {
+				if(data === parseInt(data, 10)) {
+					return data - 1;
+				}
+			});
 		},
 		render: function() {
-			segments.each(function(index, element) {
-				var box_selector = "box" + index;
+			if(this.segments.length === 0) return;
+			var that = this;
+			$.each(this.segments, function(index, element) {
+				var box_selector = "#box" + (index + 1);
 				$(box_selector).html("");
-				$(box-selector).html(this.template({text_segment: element}));
+				$(box_selector).html(that.template({text_segment: element}));
 			});
 		}
 	});
 
 
+	//App Container manages rendering
 	AppContainer = Backbone.View.extend({
 		el: "#js-app-container",
 		initialize: function(options) {
 			this.model = options.model;
+			this.list_model = options.list_model;
 			this.fb_text = options.fb_text;
 			this.lit_text = options.lit_text;
-			this.fbTextSegmentListContainer = new TextSegmentListContainer({'model': this.model, 'segments': this.fb_text});
-			this.litTextSegmentListContainer = new TextSegmentListContainer({'model': this.model, 'segments': this.lit_text});
+			this.fbTextSegmentListContainer = new TextSegmentListContainer({'model': this.model, 
+				'list_model': this.list_model, 'segments': this.fb_text});
+			this.litTextSegmentListContainer = new TextSegmentListContainer({'model': this.model, 
+				'list_model': this.list_model,'segments': this.lit_text});
 			this.fbTextSegmentListContainer.render();
 		},
 		render: function() {
-			this.textSegmentListContainer.render();
+			this.fbTextSegmentListContainer.render();
 		}
 	})
 
@@ -86,7 +127,9 @@ $(function(){
 		var that = this;
 		this.room = new Room(room_id=this.the_options.room_id);
 		this.room.once('sync', function() {
-	        var appContainer = new AppContainer({'model': that.room, 
+			this.listModel = new TextSegmentList({'room_id': this.room_id, 'model': this.room});
+			this.listModel.set({votes: [0, 0, 0, 0, 0, 0]});
+	        var appContainer = new AppContainer({'model': that.room, 'list_model': that.listModel,
 	        	'fb_text': that.fb_text, 'lit_text': that.lit_text});
 		});
 	}	
