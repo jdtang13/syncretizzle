c5abbaeb07d33eeb4fcbc3cc775881313e9d389c
Jerry Liu <jerryjliu98@gmail.com>
2015-02-22 02:23:48 -0800

    added endpoint to welcome controller to handle post request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -73,6 +73,17 @@ class WelcomeController < ApplicationController
 
   	end
 
+  	def handle_form
+  		data = params[:data]
+  		result = JSON.parse(data)
+
+  		##passes me results
+
+  		response = generate(arr1, arr2)
+  		render :json => {text: response}
+
+  	end
+
   	# Note: you need to mash F5 for this strategy to work; does not update on-screen, must manually refresh.
   	def refresh
 
